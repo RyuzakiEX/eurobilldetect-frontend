00831e69b79cb88bb29a980f759bf40c035e8158
RyuzakiEX <adanzajorge.dev@gmail.com>
2026-01-03 02:30:38 +0800

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <UContainer class="my-4">
+    <UCard>
+      <UPageHero
+        title="Euro Bill Detection"
+        description="Upload an image of a euro banknote to detect its denomination."
+      />
+
+      <UCard>
+        <UFileUpload
+          description="Upload an image of a euro banknote to detect its denomination."
+        ></UFileUpload>
+      </UCard>
+    </UCard>
+  </UContainer>
+</template>
