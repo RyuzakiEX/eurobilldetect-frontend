747c2b2e8c2ee4075e57aac57d1d52ab78eeb167
RyuzakiEX <adanzajorge.dev@gmail.com>
2026-01-04 14:35:01 +0800

    feat: improve spacing and alignment in the index page and add button for upload

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,26 @@
 <template>
   <UContainer class="my-4">
-    <UCard>
+    <UCard
+      class="border border-solid"
+      :ui="{
+        body: 'flex flex-col items-center justify-center gap-8',
+      }"
+    >
       <UPageHero
         title="Euro Bill Detection"
         description="Upload an image of a euro banknote to detect its denomination."
+        :ui="{ container: 'text-center py-10!' }"
       />
 
-      <UCard>
-        <UFileUpload
-          description="Upload an image of a euro banknote to detect its denomination."
-        ></UFileUpload>
-      </UCard>
+      <UFileUpload
+        color="neutral"
+        highlight
+        label="Drop your image here"
+        description="SVG, PNG, JPG or GIF (max. 2MB)"
+        class="w-70 sm:w-96 min-h-48"
+      />
+
+      <UButton size="xl" color="primary" label="Upload" />
     </UCard>
   </UContainer>
 </template>
